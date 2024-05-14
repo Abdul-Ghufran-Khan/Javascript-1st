94bbfc2befe5419e8e1b98d0db69cedb3a422968
Abdul-Ghufran-Khan <abdulghufran267@gmail.com>
2024-05-14 21:26:55 +0500

    completed

---
+++ app.js
@@ -0,0 +1,11 @@
+//Question 1
+alert("ASSALAM O ALAIKUM")
+//Question 2
+alert("Error! Please enter a valid password")
+//Question 3 
+alert("Welcome to  JS Land...\n Happy Coding!")
+//Question 4
+alert("Welcome to  JS Land...")
+alert("Happy Coding!\nPrevent this page from creating additional dialogs")
+//Question 5
+alert("HelLo... I can run JS through my web browser's console")
